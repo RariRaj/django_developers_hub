35d960dd12976008aa0607a4aef6897c6430b9f5
RariRaj <rariraj95@gmail.com>
2021-08-14 17:39:50 +0530

    Built all database setup(model,admin, database relationships and database queries

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,44 @@
 from django.db import models
-
+import uuid
 # Create your models here.
+class Project(models.Model):
+    #owner =
+    title = models.CharField(max_length=200)
+    description = models.TextField(null=True,blank=True)
+    #featured_image =
+    demo_link = models.CharField(max_length=1000,null=True,blank=True)
+    source_link = models.CharField(max_length=1000,null=True,blank=True)
+    vote_total = models.IntegerField(default=0)
+    vote_ratio = models.IntegerField(default=0)
+    tags = models.ManyToManyField('Tag',blank=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+
+
+    def __str__(self):
+        return self.title
+
+class Review(models.Model):
+    VOTE_TYPE = (
+        ('Up','Up'),
+        ('Down','Down'),
+    )
+    #owner =
+    project = models.ForeignKey(Project,on_delete=models.CASCADE, null=True, blank=True)
+    body = models.TextField(null=True,blank=True)
+    value = models.CharField(max_length=50,choices=VOTE_TYPE)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+
+    def __str__(self):
+        return self.value
+
+class Tag(models.Model):
+
+    name = models.CharField(max_length=100)
+    date_updated = models.DateTimeField(auto_now=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+
+    def __str__(self):
+        return self.name
--- projects/views.py
+++ projects/views.py
@@ -1,40 +1,39 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 # Create your views here.
 
-projectsList =[
-    {'id' : '1',
-    'title' : 'Ecommerce website',
-    'description' : 'fully functional shopping website',
-    'topRated' : True
-    },
-    {'id' : '2',
-    'title' : 'Todo List',
-    'description' : 'A personal website to listout things',
-    'topRated' : False
+# projectsList =[
+#     {'id' : '1',
+#     'title' : 'Ecommerce website',
+#     'description' : 'fully functional shopping website',
+#     'topRated' : True
+#     },
+#     {'id' : '2',
+#     'title' : 'Todo List',
+#     'description' : 'A personal website to listout things',
+#     'topRated' : False
 
-    },
-    {'id' : '3',
-    'title' : 'Social media',
-    'description' : 'Social media website for developers',
-    'topRated' : True
+#     },
+#     {'id' : '3',
+#     'title' : 'Social media',
+#     'description' : 'Social media website for developers',
+#     'topRated' : True
 
-    }
+#     }
 
-]
+# ]
 def home(request):
-    name = "Rari Raj C"
-    context = {'name':name, 'projectList' : projectsList}
+    projectsList = Project.objects.all()
+
+    context = {'projectsList' : projectsList}
 
     return render(request, 'projects/projects.html',context)
 
 def project(request,pk):
-    projectObject = None
-
-    for item in projectsList:
-        if item['id']== str(pk) :
-            projectObject = item
-            
-    context = {'project':projectObject}        
+    project = Project.objects.get(id=pk)
+    tags = project.tags.all()
+    reviews = project.review_set.all()
+    context = {'project':project, 'tags': tags, 'reviews':reviews }       
     
     return render(request, 'projects/project.html',context)
